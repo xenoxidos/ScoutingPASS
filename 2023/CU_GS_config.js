748704f507a9a85b2dacb167d4507cc764d3187b
xenoxidos <xenoxidos@users.noreply.github.com>
2023-03-08 22:12:27 -0500

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -1,9 +1,9 @@
 var config_data = `
 {
   "title": "Scouting PASS 2023",
+  "enable_google_sheets": "true",
   "page_title": "Charged Up",
   "checkboxAs": "10",
-  "enable_google_sheets": "true",
   "prematch": [
     { "name": "Scouter Initials",
       "code": "s",
